7b940f69dfc518bc4d87189e8e40d3af9a7b1dbd
Jonna Wiro <jonna.wiro@student.careeria.fi>
2024-11-27 13:26:22 +0200

    Controllers

--- Sanakirja/Controllers/LoginController.cs
+++ Sanakirja/Controllers/LoginController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Sanakirja.Models;
 
 namespace Sanakirja.Controllers
 {
@@ -12,7 +11,6 @@ namespace Sanakirja.Controllers
         // GET: Login
         public ActionResult Index()
         {
-            SanakirjaDBEntities db = new SanakirjaDBEntities();
             return View();
         }
     }
